53fc91ec15a908ab3a324b39328783c558d1a325
Junior Ruiz <junior31064049@gmail.com>
2024-08-19 18:00:41 -0500

    Set courses and services

---
+++ src/consts/courses.ts
@@ -0,0 +1,104 @@
+import { type Course } from "../types"
+
+export const COURSES: Course[] = [
+    {
+        id: 1,
+        title: 'Electricidad Residencial',
+        description: 'Este curso te capacita en la instalación, mantenimiento y reparación de sistemas eléctricos en viviendas, brindándote los conocimientos necesarios para realizar trabajos eléctricos seguros y eficientes.',
+    },
+    {
+        id: 2, 
+        title: 'Electricidad Industrial', 
+        description: 'Enfocado en la instalación y mantenimiento de sistemas eléctricos en entornos industriales, este curso prepara a los estudiantes para manejar equipos y sistemas eléctricos de alta potencia, asegurando un rendimiento óptimo en fábricas y plantas industriales.',
+    },
+    {
+        id: 3, 
+        title: 'Mantenimiento de Computadoras e Impresoras', 
+        description: 'Aprende a diagnosticar y reparar problemas en computadoras y sistemas de impresión, adquiriendo habilidades en hardware y software que te permitirán resolver problemas técnicos y realizar mantenimiento preventivo.',
+    },
+    {
+        id: 4, 
+        title: 'Mantenimiento de Laptops', 
+        description: 'Este curso se centra en las técnicas de reparación y mantenimiento de laptops, enseñándote a resolver problemas comunes y realizar actualizaciones para mejorar el rendimiento de estos dispositivos.',
+    },
+    {
+        id: 5, 
+        title: 'Mantenimiento de Maquinarias de Confección', 
+        description: 'Orientado a la industria textil, este curso te capacita en el mantenimiento y reparación de maquinarias utilizadas en la confección, asegurando que operen de manera eficiente y sin interrupciones.',
+    },
+    {
+        id: 6, 
+        title: 'Curso de Corte y Confección', 
+        description: 'Aprende las técnicas básicas y avanzadas de corte y confección, desde el diseño de patrones hasta la creación de prendas completas, preparándote para trabajar en la industria de la moda o para iniciar tu propio negocio.',
+    },
+    {
+        id: 7, 
+        title: 'Curso de Emprendimiento', 
+        description: 'Este curso te proporciona las herramientas y conocimientos necesarios para iniciar y gestionar tu propio negocio, cubriendo desde la creación de un plan de negocios hasta estrategias de marketing y gestión financiera.',
+    },
+    {
+        id: 8, 
+        title: 'Curso de Auxiliar de Gerencia con Aplicación de IA', 
+        description: 'Prepárate para el rol de auxiliar de gerencia, con un enfoque en la aplicación de inteligencia artificial para optimizar procesos administrativos, mejorar la toma de decisiones y gestionar eficientemente los recursos de la empresa.',
+    },
+    {
+        id: 9, 
+        title: 'Curso de Manualidades (Velas, Botellas, Flores y Otros)', 
+        description: 'Aprende a crear manualidades artísticas y decorativas utilizando una variedad de materiales, desarrollando habilidades que te permitirán emprender tu propio negocio o realizar proyectos creativos en casa.',
+    },
+    {
+        id: 10, 
+        title: 'Curso del Área Agropecuaria', 
+        description: 'Este curso te introduce a las técnicas modernas de agricultura y ganadería, enseñándote prácticas sostenibles y eficientes para maximizar la producción y cuidar el medio ambiente.',
+    },
+    {
+        id: 11, 
+        title: 'Curso de Bomberos Profesionales', 
+        description: 'Capacítate como bombero profesional, adquiriendo conocimientos en la prevención, control y extinción de incendios, así como en rescates y manejo de emergencias, con un enfoque en la seguridad y el servicio comunitario.',
+    },
+    {
+        id: 12, 
+        title: 'Curso de Seguridad Laboral e Industrial', 
+        description: 'Aprende sobre las normativas y prácticas de seguridad en el entorno laboral e industrial, preparándote para identificar y mitigar riesgos, asegurando un ambiente de trabajo seguro y conforme a la legislación vigente.',
+    },
+    {
+        id: 13, 
+        title: 'Power BI, Query, Pivot, SAP', 
+        description: 'Aprende a utilizar herramientas clave como Power BI, Power Query, Power Pivot y SAP para transformar datos en visualizaciones interactivas, automatizar la limpieza y transformación de datos, gestionar procesos empresariales y realizar análisis avanzados, optimizando la toma de decisiones en tu organización.',
+    },
+    {
+        id: 14, 
+        title: 'Excel (Básico, Intermedio, Avanzado)', 
+        description: 'Ofrecemos una formación integral en Excel, desde los fundamentos básicos hasta técnicas avanzadas como la automatización con macros y la creación de informes dinámicos. Este curso está diseñado para usuarios de todos los niveles que desean mejorar sus habilidades en una de las herramientas más utilizadas en el entorno laboral.',
+    },
+    {
+        id: 15, 
+        title: 'Desarrollo Web', 
+        description: 'Este curso ofrece una formación integral en desarrollo web, cubriendo desde los fundamentos de HTML, CSS y JavaScript, hasta tecnologías avanzadas como frameworks de front-end (React, Angular) y back-end (Node.js). Los estudiantes aprenderán a crear sitios web dinámicos y responsivos, desarrollando aplicaciones web que cumplen con los estándares modernos de usabilidad y rendimiento.',
+    },
+    {
+        id: 16, 
+        title: 'Programación con Python', 
+        description: 'Python es uno de los lenguajes de programación más versátiles y demandados en la industria tecnológica. Este curso cubre desde los conceptos básicos de programación hasta aplicaciones avanzadas como el análisis de datos, desarrollo web, y automatización de tareas. Ideal tanto para principiantes como para aquellos que desean ampliar sus conocimientos en programación, Python es una herramienta esencial en el arsenal de cualquier desarrollador.',
+    },
+    {
+        id: 17, 
+        title: 'Uso de la IA en Educación', 
+        description: 'Descubre cómo la inteligencia artificial puede transformar la educación. Este curso explora aplicaciones prácticas de la IA en la enseñanza y el aprendizaje, proporcionando herramientas para mejorar la eficiencia y personalización en la educación.',
+    },
+    {
+        id: 18, 
+        title: 'Uso de la IA en Gerencia', 
+        description: 'Aprende a aplicar la inteligencia artificial en la gestión empresarial para optimizar procesos, mejorar la toma de decisiones y aumentar la eficiencia operativa. Este curso está dirigido a profesionales que buscan integrar tecnologías emergentes en su estrategia de gestión.',
+    },
+    {
+        id: 19, 
+        title: 'Uso de la IA en Marketing', 
+        description: 'Este curso te enseñará cómo utilizar la inteligencia artificial para mejorar las estrategias de marketing, desde la segmentación de mercado hasta la personalización de campañas publicitarias, logrando un mayor impacto y retorno de inversión.',
+    },
+    {
+        id: 20, 
+        title: 'Otros Cursos Adicionales', 
+        description: 'Además, ofrecemos una variedad de cursos adicionales en diferentes áreas especializadas, todos diseñados para equiparte con las habilidades necesarias para destacar en tu campo de interés.',
+    },
+]
