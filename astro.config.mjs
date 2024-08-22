bbf9b80bc4faa9a74f9562a46c934bbac5b9afe0
Junior Ruiz <junior31064049@gmail.com>
2024-08-22 18:31:44 -0500

    vercel adapter added to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(), icon(), react()]
+  integrations: [tailwind(), mdx(), sitemap(), icon(), react()],
+  output: "server",
+  adapter: vercel()
 });
